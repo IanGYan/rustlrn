63f061ab3b7340a21abcfdc699e1cf64cee71543
IanYan <ian@MacIan.local>
2021-03-22 14:16:39 +0800

    v0.1.1 added Error handle for invalid UTF-8 input.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,55 @@
-/*
-Echo Server
-v0.1.0
-A TCP Server which can echo the connection.
-Usage: echosrv
+/// echosrv is a TCP Server which echo the input back to Terminal.
+/// Current version only supoort UTF-8.
 
-Author: Ian Yan @ian@adhub.cn
-Date: 2021-Mar-20
-*/
-
+/// Use std Lib only.
 use std::{
     io::{self, prelude::*},
     error, net, str, thread,
   };
 
   /// Main function
-  fn main() -> Result<(), Box<dyn error::Error>>{   // Throw exceptions
-    let listener = net::TcpListener::bind("127.0.0.1:7878")?;   // Monitoring port 7878
+  fn main() -> Result<(), Box<dyn error::Error>>{
+    // Monitoring port 7878
+    let listener = net::TcpListener::bind("127.0.0.1:7878")?;
     loop {
-      let (stream, _) = listener.accept()?; // Block the calling thread until accept new data from tcp stream
-      thread::spawn(move || {   //Create a thread to handle the echo
-        handler(stream).unwrap();   // Handle the echo
+      // Block the calling thread until accept new data from tcp stream
+      let (stream, _) = listener.accept()?;
+      // Spawn a new thread to handle the stream.
+      thread::spawn(move || {
+        // Call the handler() function to echo client.
+        handler(stream).unwrap()
       });
     }
   }
 
-  /// Function echo will send back the received data to client
+  /// Function handler() will send back the received data to client by line.
   fn handler(mut stream: net::TcpStream) -> Result<(), Box<dyn error::Error>> {
-    println!("Receive data from {}", stream.peer_addr()?);  // Print the source addr
+    // Print the connection info
+    println!("{} Connected!", stream.peer_addr()?);
     loop {
-      let mut reader = io::BufReader::new(&stream); // Store the stream to BufReader
-      let mut buf = vec![]; // Create a buffer to format the received data
-      match reader.read_until(b'\n', &mut buf)? {   // from BufReader store data to buffer until '\n' or EOF
-        0 => {  //  EOF
-          println!("Connection closed");    // Print Closed info to terminal
-          return Ok(()) // Return Ok Result
+      // Create a BufReader to store the stream
+      let mut reader = io::BufReader::new(&stream);
+      // Create a Vector buf to handle the incoming data.
+      let mut buf = Vec::<u8>::new();
+      // Get a line to buf
+      match reader.read_until(b'\n', &mut buf)? {
+        0 => {  //  If can not read anything from stream
+          // Print connection closed info to the Terminal.
+          println!("Connection closed!");
+          // Return Ok Result.
+          return Ok(())
         },
-        n => {  // End of Line
-          print!("{}", str::from_utf8(&buf[..n])?); // Convert buffer to String and print
-          stream.write_all(&buf[..n])?; // Write back the data to Client
+        n => {  // Encount the EOL(\n)
+          // Convert the buf to UTF-8 String
+          let line = str::from_utf8(&buf[..n]);
+          // Use patther to handle the Error
+          match line {
+            Ok(line) => { // If the line is a valid UTF-8 String, print it and write back to client.
+              print!("{}: {}", stream.peer_addr()?, line);
+              stream.write_all(&buf[..n])?
+            },
+            Err(err) => print!("Error: {}", err), // If the line is a invalid UTF-8 String, print Error info.
+          }
         }
       }
     }
